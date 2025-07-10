0a599394943508e2e5a83fcf8eef0550f826b1b2
yjl-jp <yingjingliang24@gmail.com>
2025-07-10 22:02:51 +0800

    enemy state

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,53 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRender;
-    private void Awake()
-    {
+
+    private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
     }
-    private void OnEnable()
-    {
+
+    private void OnEnable() {
         playerControls.Enable();
     }
-    private void Update()
-    {
-        PlayerIuput();
+
+    private void Update() {
+        PlayerInput();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         AdjustPlayerFacingDirection();
         Move();
-       
     }
-    private void PlayerIuput()
-    {
+
+    private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
-    private void Move()
-    {
-        rb.MovePosition(rb.position + movement*(moveSpeed*Time.fixedDeltaTime));
+
+    private void Move() {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
-    private void AdjustPlayerFacingDirection()
-    {
+
+    private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        if (mousePos.x<playerScreenPoint.x)
-        {
+
+        if (mousePos.x < playerScreenPoint.x) {
             mySpriteRender.flipX = true;
-        }
-        else
-        {
+        } else {
             mySpriteRender.flipX = false;
         }
     }
